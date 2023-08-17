63b5aa954d70ed375dbb8c5e0b478c28c3a77eb5
dzxdddd <142412124+dzxdddd@users.noreply.github.com>
2023-08-17 08:55:55 +0800

    Add files via upload

---
+++ app.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+from flask import Flask,render_template,request
+import openai
+
+openai.api_key = "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"
+app = Flask(__name__)
+
+@app.route("/",methods=["GET","POST"])
+def index():
+    if request.method == "POST":
+        q = request.form.get("question")
+        r = openai.ChatCompletion.create(
+            model = "gpt-3.5-turbo",
+            messages = 
+                [
+                    {
+                        "role":"user",
+                        "content":q
+                    }
+                ]
+        )
+        return(render_template("index.html",result=r["choices"][0]["message"]["content"]))
+    else:
+        return(render_template("index.html",result="waiting for your question......"))
+
+if __name__ == "__main__":
+    app.run()
+
+
+# In[ ]:
+
+
+
+
